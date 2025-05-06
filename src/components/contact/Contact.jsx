0098b2a22c75134922dc435934a593450eef9cbb
Lili <Liliia_Nel@proton.me>
2025-05-06 23:54:36 +0200

    add icons

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,12 @@
 import css from "./Contact.module.css";
+import { IoMdPerson } from "react-icons/io";
+import { MdLocalPhone } from "react-icons/md";
 
 export default function Contact({ contact, deleteItem }) {
                 return <li key={contact.id} className={css.item}>
                      <div className={css.info}>
-                        <p>{contact.name}</p>
-                        <p>{contact.number}</p>
+                        <p><IoMdPerson /> {contact.name}</p>
+                        <p><MdLocalPhone /> {contact.number}</p>
                     </div>
                     <button className={css.button} type="button" onClick={() => { deleteItem (contact.id)}}>Delete</button>
                 </li>;
